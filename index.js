88b79e4245dad940650c56cf3d34b579b466a9f3
Jessica Wilson <jwilson@zipwhip.com>
2020-01-06 16:05:20 -0800

    Added workflow

---
+++ index.js
@@ -0,0 +1,48 @@
+const core = require('@actions/core');
+const { exec } = require('child_process');
+
+try {
+  const branchPrefix = core.getInput('branch-prefix');
+  const semanticVersion = core.getInput('semantic-version');
+  const branchName = branchPrefix + semanticVersion;
+  const regexp = /^[A-Za-z0-9_-]*$/;
+  if (regexp.test(branchName)) {
+    const commands = 'git checkout -b "${branchName}" &&\ git push --set-upstream origin "${branchName}" &&\ echo "Created " ${branchName}'  
+    console.log("commands", commands)
+    // const output = cutReleaseBranch(branchName);
+    // output.then(function(result){
+    //   if (result["semanticVersion"]) {
+    //     console.log('\x1b[32m%s\x1b[0m', `Last Semantic Version Found: ${result["semanticVersion"]}`);
+    //     core.setOutput("last-semver", result["semanticVersion"]);
+    //   }
+    // });
+  } else {
+    const regexError = "Branch prefix and semantic version must contain only numbers, strings, underscores, and dashes.";
+    console.log('\x1b[33m%s\x1b[0m', regexError);
+    core.setFailed(regexError);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+// function cutReleaseBranch(command) {
+//   try{
+//     const { err, stdout, stderr } = exec(command, [{ shell: "bash" }]);
+
+//     if (err) {
+//       console.log('\x1b[33m%s\x1b[0m', 'Could not find any branches because: ');
+//       console.log('\x1b[31m%s\x1b[0m', stderr);
+//       process.exit(1);
+  
+//       return;
+//     }
+  
+//     const data = JSON.parse(stdout);
+//     if (data) {
+//       return data;
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     process.exit(0);
+//   }
+// }
