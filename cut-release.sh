d46414f530b662490c00ee00575ebd8b38155fe4
Jessica Wilson <jwilson@zipwhip.com>
2020-01-09 15:33:34 -0800

    debug

--- cut-release.sh
+++ cut-release.sh
@@ -5,7 +5,8 @@ set -eou pipefail
 branchName=$1
 repositoryUrl=$2
 
-echo $(git checkout -b $branchName)
-echo $(git remote set-url origin $repositoryUrl)
-echo $(git push --set-upstream origin $branchName)
+git checkout -b $branchName
+git remote set-url origin $repositoryUrl
+# echo 'Set remote url origin to '$repositoryUrl
+git push --set-upstream origin $branchName
 echo 'Created '$branchName' in '$repositoryUrl
