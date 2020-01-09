44d3f89adcfa4f71d9a6419039fa20872235de9d
Jessica <jessicalostinspace@gmail.com>
2020-01-09 14:25:03 -0800

    Development (#1)
    * Added workflow
    
    * Added in bash script
    
    * Added error logging, new version of bum semver
    
    * Passed commands to exec
    
    * Passing in workflow vars
    
    * Added correct step var
    
    * Added periods to accepted branch name
    
    * Added logging to bash script
    
    * Adding core logging
    
    * Added bash script
    
    * Removed child process import
    
    * Added actions/exec package
    
    * Added core package
    
    * Passed in branch name to script
    
    * Added perms to script
    
    * added output log
    
    * Adding token and secrets to workflow, scripts
    
    * Removed environment variables
    
    * Moving env vars solely to workflow file
    
    * Passed in repo url to bashs cript:
    
    * Changed url syntax
    
    * Added github vars to wrokflow
    
    * Set remote url
    
    * Clean up env vars
    
    * Added url description
    
    * added new version of get-regex-branch action
    
    * Updated sem branch action version
    
    * bumped semver action ver

---
+++ cut-release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eou pipefail
+
+branchName=$1
+repositoryUrl=$2
+
+git checkout -b $branchName
+git remote set-url origin $repositoryUrl
+echo 'Set remote url origin to '$repositoryUrl
+git push --set-upstream origin $branchName
+echo 'Created '$branchName' in '$repositoryUrl
---
+++ index.js
@@ -0,0 +1,59 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+const src = __dirname;
+console.log("src: ", src)
+core.debug(`src: ${src}`);
+
+try {
+  const branchPrefix = core.getInput('branch-prefix');
+  const semanticVersion = core.getInput('semantic-version');
+  const branchName = branchPrefix + semanticVersion;
+
+  const repositoryUrl = core.getInput('repository-url');
+  console.log('repository-url', repositoryUrl);
+
+  const regexp = /^[\.A-Za-z0-9_-]*$/;
+  if (regexp.test(branchName)) {
+    const output = cutReleaseBranch(branchName, repositoryUrl);
+    output.then(function(result){
+        console.log("result: ", result)
+    //   if (result["semanticVersion"]) {
+    //     console.log('\x1b[32m%s\x1b[0m', `Last Semantic Version Found: ${result["semanticVersion"]}`);
+        // core.setOutput("last-semver", result["semanticVersion"]);
+    //   }
+    });
+  } else {
+    const regexError = "Branch prefix and semantic version must contain only numbers, strings, underscores, periods, and dashes.";
+    console.log('\x1b[33m%s\x1b[0m', regexError);
+    core.setFailed(regexError);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+async function cutReleaseBranch(branchName, repositoryUrl) {
+  try{
+    const execOutput = await exec.exec(`${src}/cut-release.sh ${branchName} ${repositoryUrl}`);
+    execOutput.then(function(result){
+        console.log("exec Output result: ", result)
+    });
+    // core.debug("execOutput:", execOutput.then((result) => console.log("result", result)))
+
+    if (err) {
+      console.log('\x1b[33m%s\x1b[0m', 'Could not create new release branch because: ');
+      console.log('\x1b[31m%s\x1b[0m', stderr);
+      process.exit(1);
+  
+      return;
+    }
+
+    // const data = JSON.parse(stdout);
+    // if (data) {
+    //   return data;
+    // }
+  } catch (err) {
+    console.log(err);
+    process.exit(0);
+  }
+}
