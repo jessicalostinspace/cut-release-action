8a5b4b3616d6e612805c5906018b7698205e28c9
Jessica <jessicalostinspace@gmail.com>
2020-01-09 15:52:32 -0800

    Development (#2)
    Reverted to action/checkout ver 1 + clean up

--- cut-release.sh
+++ cut-release.sh
@@ -7,6 +7,5 @@ repositoryUrl=$2
 
 git checkout -b $branchName
 git remote set-url origin $repositoryUrl
-echo 'Set remote url origin to '$repositoryUrl
 git push --set-upstream origin $branchName
 echo 'Created '$branchName' in '$repositoryUrl
--- index.js
+++ index.js
@@ -2,30 +2,18 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 
 const src = __dirname;
-console.log("src: ", src)
-core.debug(`src: ${src}`);
 
 try {
   const branchPrefix = core.getInput('branch-prefix');
   const semanticVersion = core.getInput('semantic-version');
   const branchName = branchPrefix + semanticVersion;
-
   const repositoryUrl = core.getInput('repository-url');
-  console.log('repository-url', repositoryUrl);
-
   const regexp = /^[\.A-Za-z0-9_-]*$/;
+
   if (regexp.test(branchName)) {
-    const output = cutReleaseBranch(branchName, repositoryUrl);
-    output.then(function(result){
-        console.log("result: ", result)
-    //   if (result["semanticVersion"]) {
-    //     console.log('\x1b[32m%s\x1b[0m', `Last Semantic Version Found: ${result["semanticVersion"]}`);
-        // core.setOutput("last-semver", result["semanticVersion"]);
-    //   }
-    });
+    cutReleaseBranch(branchName, repositoryUrl);
   } else {
     const regexError = "Branch prefix and semantic version must contain only numbers, strings, underscores, periods, and dashes.";
-    console.log('\x1b[33m%s\x1b[0m', regexError);
     core.setFailed(regexError);
   }
 } catch (error) {
@@ -33,27 +21,32 @@ try {
 }
 
 async function cutReleaseBranch(branchName, repositoryUrl) {
-  try{
-    const execOutput = await exec.exec(`${src}/cut-release.sh ${branchName} ${repositoryUrl}`);
-    execOutput.then(function(result){
-        console.log("exec Output result: ", result)
-    });
-    // core.debug("execOutput:", execOutput.then((result) => console.log("result", result)))
+  try {
+    let output = '';
+    let err = '';
 
-    if (err) {
-      console.log('\x1b[33m%s\x1b[0m', 'Could not create new release branch because: ');
-      console.log('\x1b[31m%s\x1b[0m', stderr);
+    const options = {};
+    options.listeners = {
+      stdout: (data) => {
+        output += data.toString();
+      },
+      stderr: (data) => {
+        err += data.toString();
+      }
+    };
+    options.cwd = './';
+
+    await exec.exec(`${src}/cut-release.sh`, [branchName, repositoryUrl], options);
+
+    if (output) {
+      console.log('\x1b[32m%s\x1b[0m', `Github Output: ${output}`);
+      core.setOutput("release-branch-name", branchName);
+    } else {
+      core.setFailed(err);
       process.exit(1);
-  
-      return;
     }
-
-    // const data = JSON.parse(stdout);
-    // if (data) {
-    //   return data;
-    // }
   } catch (err) {
-    console.log(err);
+    core.setFailed(`Could not cut release branch because: ${err.message}`);
     process.exit(0);
   }
 }
